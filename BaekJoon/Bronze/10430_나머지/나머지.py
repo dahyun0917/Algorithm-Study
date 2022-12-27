6526c5c909ee166700728a50cd61e9c787d87f0b
Lee Dahyun <75965656+dahyun0917@users.noreply.github.com>
2022-12-28 01:34:21 +0900

    [Bronze V]

---
+++ BaekJoon/Bronze/10430_나머지/나머지.py
@@ -0,0 +1,8 @@
+#BackJoon 10430 나머지
+
+input = list(map(int,input().split()))
+
+print((input[0]+input[1])%input[2])
+print(((input[0]%input[2])+(input[1]%input[2]))%input[2])
+print((input[0]*input[1])%input[2])
+print(((input[0]%input[2])*(input[1]%input[2]))%input[2])
